d8e49219be83315eeb086b28f4aae7a7b01dc38b
Abhishek Kumar <kumar.abh@husky.neu.edu>
2015-04-09 19:36:50 -0400

    commiting server.js changes.

--- server.js
+++ server.js
@@ -14,4 +14,10 @@ app.get('/', function(req, res){
   res.send('hello world');
 });
 
-app.listen(3000);
+
+/* CONNECTION STRING */
+
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.listen(port, ip);
